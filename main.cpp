2d5f9be0eeced238876ed5612f94ebae22d1109b
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2022-08-28 21:12:47 +0000

    From vectors to maps (associated arrays)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,30 @@
 #import <vector> 
-using std::vector;
+using std::vector; //deprecated 
 #import <map>
 using std::map;
 
-#import <iostream>
+// for debugging 
+#import <iostream> 
 
-const int MYIP = 6689;  //for referance and scalablity
-const int MYELO = 56678; //for referance and scalablity
+// IPs are 4 bytes (4 0-255 values) and an int is 4 bytes so makes sense to convert them like this, 
+// LAN connections do make this more complicated and suggest use of a struct, psuedo-coded below
+struct IP {
+  int publicIP;
+  char LANid; 
+};
+
+
+const int MY_ELO = 56678; //for referance and scalablity
 
 struct packet {    //once created should be immutable
   private: 
-    int ip;  //type? 
-    int elo; 
+    uint ip;  
+    uint elo; 
   public:
     // elo => elo; 
-    int getIp(){return ip;}
-    int getElo(){return elo;}
-    packet(int Ip, int Elo)
+    uint getIp(){return ip;}
+    uint getElo(){return elo;}
+    packet(uint Ip, uint Elo)
     {
       ip = Ip;
       elo = Elo;      
@@ -24,15 +32,18 @@ struct packet {    //once created should be immutable
     bool const operator < (const packet p){return elo < p.elo; }
 };
 
+
 // Ranges of ranks (bronze, silver, gold etc) 
 // Placed in array as ranks don't need to be equally spaced
 const int RANK_NUM = 4;
 const int RANKS[RANK_NUM+1] = {0, 10, 20, 30, 40 }; // each item represents the lower bound of the rank
-packet my_packet = packet(MYIP, MYELO);
-const int ELO_LOWERBOUND = my_packet.getElo() * 0.9;
-const int ELO_UPPERBOUND = my_packet.getElo() * 1.1;
+// packet my_packet = packet(0, MY_ELO); //client doesn't need referance to own IP
+const uint ELO_LOWERBOUND = MY_ELO * 0.9;
+const uint ELO_UPPERBOUND = MY_ELO * 1.1;
 
-vector<packet> database[RANK_NUM]; // one for each rank, each should be sorted
+// vector<packet> database[RANK_NUM]; // one for each rank, each should be sorted
+map<int,int> database; 
+
 vector<packet> possible_matches; // the players 
 
 vector<packet>::iterator sorted_pos(vector<packet> vec, int item)
@@ -41,7 +52,6 @@ vector<packet>::iterator sorted_pos(vector<packet> vec, int item)
   int min = 0; int max = vec.size();  
   while(max-min>1)
     {
-      // (vec[key].getElo() > item)
        if(vec[key].getElo() > item)
       { 
         min = key; 
@@ -55,9 +65,17 @@ vector<packet>::iterator sorted_pos(vector<packet> vec, int item)
     } // end while 
   return vec.begin() + min;
 }
-void sorted_insert(map<int,int> & M, int item)
+
+bool database_insert(map<int,int> & M, packet p)
 {
-  // if M.item
+  uint item = p.getElo();
+  if (std::distance(M.find(item), M.end()) > 0) //contains is c++ 20
+  {
+    // key is not in map
+    M[item] = p.getIp(); //add item to map
+    return true;
+  }
+  return false;
 }
 
 void check_elo( packet query )
@@ -65,22 +83,13 @@ void check_elo( packet query )
   int q_elo = query.getElo();
   if (q_elo > ELO_LOWERBOUND && q_elo < ELO_UPPERBOUND)
   {
-    possible_matches.emplace_back(query.getIp(),q_elo); // not a sorted vector
+    //Add to my possible matches
+    possible_matches.emplace_back(query.getIp(),q_elo); 
   }
-  for (int i=0; i < RANK_NUM; i++)
-    {
-      if (q_elo > RANKS[i+1])
-      {
-        database[i].emplace(sorted_pos(database[i],q_elo), query.getIp() , q_elo);
-        // UDP.send(database[i].size()); // other end needs to know size of message to recive it 
-        // UDP.send(database[i]); // now we can send the data
-        break;
-      }
-    }
-  
+  database_insert(database, query);
 } 
 
 int main(){
-  std::cout<<"success! :) "<<"\n";
+  std::cout<<"success! :)" << "\n";
 }
   
