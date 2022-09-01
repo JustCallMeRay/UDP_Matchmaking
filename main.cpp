01e8b5ef14d266b35885865f10f7de1b7cfa02f5
Ray <57572379+JustCallMeRay@users.noreply.github.com>
2022-09-01 18:29:05 +0000

    better handshaking/ naming

--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@ using std::vector;
 #import <map>
 using std::map;
 
-#import <algorithm>
-
 // for debugging
 #import <iostream> 
 using std::cout;
 
-// IPs are 4 bytes (4 0-255 values) and an int is 4 bytes so makes sense to convert them like this, 
-// LAN connections do make this more complicated and suggest use of a struct so I included one for scalability.
+#import <string>
+using std::string;
+
+#import <algorithm>
+// IPs are 4 bytes (4 * 0-255 values) and an int(32) is 4 bytes so makes sense to convert them 
+// LAN connections do make this more complicated and suggests use of a struct so I included one for scalability.
 struct IP {
   uint publicIP;
   char LANid; 
@@ -24,8 +26,7 @@ struct IP {
 
 };
 
-
-struct packet {    //once created should be immutable
+struct Packet {    //once created should be immutable
   private: 
     IP ip;  
     uint elo; 
@@ -33,41 +34,52 @@ struct packet {    //once created should be immutable
     // elo => elo; 
     IP getIp(){return ip;}
     uint getElo(){return elo;}
-    packet(uint publicIP, char Lanid, uint Elo){
+    Packet(uint publicIP, char Lanid, uint Elo){
       ip = IP(publicIP, Lanid); elo = Elo; 
     }
-    packet(IP Ip, uint Elo){
+    Packet(IP Ip, uint Elo){
       ip = Ip;
       elo = Elo;      
     }
-    packet(){}
-    bool const operator < (const packet p){return elo < p.elo; }
+    Packet(){}
+    bool const operator < (const Packet p){return elo < p.elo; }
+};
+
+class Bounds 
+{
+  public: 
+    uint Lb;
+    uint Ub;
+    Bounds(){}
+    Bounds(int Lower, int Upper){
+      Lb = Lower; Ub = Upper;
+    }
+    bool const operator == (const uint c){
+      return (c < Ub && c > Lb);
+    }
+    
 };
 
 const int MY_ELO = 56678; //for referance and scalablity
 
 
-// Ranges of ranks (bronze, silver, gold etc) 
-// Placed in array as ranks don't need to be equally spaced
-const int RANK_NUM = 4;
-const int RANKS[RANK_NUM+1] = {0, 10, 20, 30, 40 }; // each item represents the lower bound of the rank
-// packet my_packet = packet(0, MY_ELO); //client doesn't need referance to own IP
+Bounds bounds = Bounds(MY_ELO * 0.9, MY_ELO * 1.1);
 const uint ELO_LOWERBOUND = MY_ELO * 0.9;
 const uint ELO_UPPERBOUND = MY_ELO * 1.1;
 
-// vector<packet> database[RANK_NUM]; // one for each rank, each should be sorted
+// vector<Packet> database[RANK_NUM]; // one for each rank, each should be sorted
 map<int,IP> database; 
 
 // to be deprecated, look ups in map instead
-vector<packet> possible_matches; // the players 
+vector<Packet> possible_matches; // the players 
 
 //to be depricated
-bool database_insert(map<int,IP> & M, packet p)
+bool database_insert(map<int,IP> & M, Packet p)
 {
   uint item = p.getElo();
   if (std::distance(M.find(item), M.end()) > 0) 
   {
-    // key is not in map
+    // key is not in database
     M[item] = p.getIp(); //add item to map
     return true;
   }
@@ -76,7 +88,7 @@ bool database_insert(map<int,IP> & M, packet p)
 }
 
 //Run when packet comes in, if the value is within the bounds for a match, adds it to all possible matches, always adds it to database. 
-void check_elo( packet query )
+void check_elo( Packet query )
 {
   int q_elo = query.getElo();
   if (q_elo > ELO_LOWERBOUND && q_elo < ELO_UPPERBOUND)
@@ -90,38 +102,65 @@ void check_elo( packet query )
 }  
 
 
-//Run as soon as a packet comes in, returns all the values in it's database that are a possible match for the incomming packet. 
-map<int,IP> questioned(packet q, uint lower_bound, uint upper_bound)
-{
-  map<int,IP> result = {};
-  auto LBit = database.lower_bound(lower_bound);
-  auto UBit = database.upper_bound(lower_bound);
-  //return all the usefull values from our database. 
-  std::copy(LBit, UBit, std::inserter(result, result.begin()));
-  database_insert(database, q);
 
-  return result;
-}
+
+
 
 bool check_matchfound()
 {
   bool b = false;
-  //tell all of the clients we found a match and await response
-  //set a flag so incomming UDP connections know we're in a match
+  // tell all of the clients we found a match and await response
+  // set a flag so incomming UDP connections know we're in a match
   // awaiting_reponse ? continue : ( matchfound ? b= true, continue : return false)
-  //write database to file so we can remove it from the heap
+  // write database to file so we can remove it from the heap
   
-  // b ? return true : continue
+  
   //check repsonse from match clients again
+  // b ? return true : continue
   
   //return b
 
   return true; 
 }
 
+void send(uint elo, char h_rand, Bounds b = bounds ){ /* not yet implimented*/ }
+void send(int size, char c){ /* not yet implimented*/ }
+void send(map<int,IP> data, char c){ /* not yet implimented*/ }
+void send(char c){/* end */}
+
+char handshake_rand = 26;
+void SYN()
+{
+  //send UDP packet 
+  send(MY_ELO, handshake_rand, bounds);
+}
+
+//Run as soon as a packet comes in, returns all the values in it's database that are a possible match for the incomming packet. SYN-ACK refers to synchronize-acknowledgment in TCP handshake
+/* map<int,IP> */void SYN_ACK(Packet q, uint lower_bound, uint upper_bound, char h_rand)
+{
+  map<int,IP> result = {};
+  auto LBit = database.lower_bound(lower_bound);
+  auto UBit = database.lower_bound(upper_bound);
+  
+  //return all the usefull values from our database. 
+  std::copy(LBit, UBit, std::inserter(result, result.begin()));
+  check_elo(q);
+  // (int) result.size() * (IP.size() + Int.size())
+  send((int) result.size() , h_rand+1);
+  send(result, h_rand +2);
+  // return result;
+}
+
+void ACK(char x)
+{
+  send(x+1);
+}
 
 
 int main(){
+  
+
+  
   cout<<"successfully compiled! 👍😁" << "\n";
 }
   
